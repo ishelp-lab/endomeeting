1ca9b007a8170cb26f6ab294e9f624f77d7afc9a
Fhelipe Abel <processos@assessoriaconverte.com.br>
2026-04-28 16:04:21 -0300

    Correção

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
-export const dynamic = "force-dynamic";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
